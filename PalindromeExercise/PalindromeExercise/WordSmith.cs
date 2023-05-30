216ba7ad9689bbf5e69c6490c75cf872d1dc3fd5
AlissaStegall <alissaistegall@gmail.com>
2023-05-30 16:27:07 -0700

    completed palindrome tests

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+			/*word = word.ToLower();
+			var reversed = "";
+
+			for(int i = word.Length -1; i >= 0; i--)
+			{
+				reversed += word[i];
+			}
+
+			if(reversed == word)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}*/
+
+			word = word.ToLower();
+			return word.SequenceEqual(word.Reverse());
+
+		}
+		
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("radar", true)]
+        [InlineData("dummy", false)]
+        [InlineData("RaceCar", true)]
+        public void PTest(string word, bool expected)
         {
+            var test1 = new WordSmith();
 
+            var actual = test1.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
